e33d0a6af2dde0d29d6437e42e7729f07454c49e
sentairanger <ed2point0@gmail.com>
2022-04-26 19:30:18 -0600

    Add files via upload

---
+++ robot-forward.py
@@ -0,0 +1,20 @@
+from gpiozero import OutputDevice
+from gpiozero.pins.pigpio import PiGPIOFactory
+from signal import pause
+
+factory = PiGPIOFactory(host='192.168.0.10')
+
+pin1 = OutputDevice(7,  pin_factory = factory)
+pin2 = OutputDevice(8,  pin_factory = factory)
+pin3 = OutputDevice(9,  pin_factory = factory)
+pin4 = OutputDevice(10,  pin_factory = factory)
+
+def forward():
+    pin1.off()
+    pin2.on()
+    pin3.on()
+    pin4.off()
+
+forward()
+
+pause()
---
+++ robot-stop.py
@@ -0,0 +1,18 @@
+from gpiozero import OutputDevice
+from gpiozero.pins.pigpio import PiGPIOFactory
+
+factory = PiGPIOFactory(host='192.168.0.10')
+
+pin1 = OutputDevice(7,  pin_factory = factory)
+pin2 = OutputDevice(8,  pin_factory = factory)
+pin3 = OutputDevice(9,  pin_factory = factory)
+pin4 = OutputDevice(10,  pin_factory = factory)
+
+def stop():
+    pin1.off()
+    pin2.off()
+    pin3.off()
+    pin4.off()
+
+
+stop()
